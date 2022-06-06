9134dd240a3c654849e25451e10672d6a3c2555e
kecheste <aben.ezer443@gmail.com>
2022-06-06 13:52:32 -0700

    g

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,4 +5,4 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        my_list.replace(idx, element)
+        return my_list.replace(idx, element)

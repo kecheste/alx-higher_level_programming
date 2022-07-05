6df8ed38ba1135cf3cc9f6fbbf560f591bcfa4ba
kecheste <aben.ezer@gmail.com>
2022-07-05 22:10:33 +0000

    g

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Module 14-pascal_triangle.
 Returns a list of lists of integers
-representing the Pascal’s triangle of n.
+representing the Pascal's triangle of n.
 """
 
 
 def pascal_triangle(n):
     """Returns the pascal triangle of n.
+
     Args:
         - n: size of the triangle (rows)
+
     Returns: a list of list of integers
     """
 
